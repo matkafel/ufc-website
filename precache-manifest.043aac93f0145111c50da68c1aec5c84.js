1e6705aa03b1dfa68fa3b2344f670394badd5987
unknown <mat.kafel@gmail.com>
2020-10-26 11:14:00 +0100

    Updates

---
+++ precache-manifest.043aac93f0145111c50da68c1aec5c84.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf18b0ef6132c7002e9b72723dca664",
+    "url": "/ufc-website/index.html"
+  },
+  {
+    "revision": "9244c6b13c939ff8d8d4",
+    "url": "/ufc-website/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "b986a784b56c006f791f",
+    "url": "/ufc-website/static/css/main.5b28880a.chunk.css"
+  },
+  {
+    "revision": "9244c6b13c939ff8d8d4",
+    "url": "/ufc-website/static/js/2.42b9c9a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ufc-website/static/js/2.42b9c9a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b986a784b56c006f791f",
+    "url": "/ufc-website/static/js/main.56527286.chunk.js"
+  },
+  {
+    "revision": "6dfb2dcc187bec735093",
+    "url": "/ufc-website/static/js/runtime-main.1cc2bf11.js"
+  },
+  {
+    "revision": "8adcacd7dfd895c3054990bf1e82801d",
+    "url": "/ufc-website/static/media/Khabib.8adcacd7.png"
+  },
+  {
+    "revision": "6412a64f4619859cc3437bdd2b3bbee5",
+    "url": "/ufc-website/static/media/UFC254.6412a64f.jpg"
+  },
+  {
+    "revision": "f766d30acc78018ea733490116ae4c25",
+    "url": "/ufc-website/static/media/UFC255.f766d30a.jpg"
+  },
+  {
+    "revision": "0208dbf3e5313778b93bddef1562d6e8",
+    "url": "/ufc-website/static/media/UFC256.0208dbf3.jpg"
+  },
+  {
+    "revision": "8558d06029745dd0f8de2240c1b54656",
+    "url": "/ufc-website/static/media/Yan.8558d060.png"
+  },
+  {
+    "revision": "f54ef0f67be8fcf262c7a44f7156ebfe",
+    "url": "/ufc-website/static/media/adesanya.f54ef0f6.png"
+  },
+  {
+    "revision": "318743d5b1a1e02612212ee46539acbd",
+    "url": "/ufc-website/static/media/australia.318743d5.jpg"
+  },
+  {
+    "revision": "b05ea2a18e7a1e520b8bffacb959ce90",
+    "url": "/ufc-website/static/media/blachowicz.b05ea2a1.png"
+  },
+  {
+    "revision": "3547436372109b8e380c0f41aed7af13",
+    "url": "/ufc-website/static/media/brazil.35474363.jpg"
+  },
+  {
+    "revision": "c9e7f15d0be661205e50a9405f77d799",
+    "url": "/ufc-website/static/media/china.c9e7f15d.jpg"
+  },
+  {
+    "revision": "051a17c0174f3d43983d5e3889515ff8",
+    "url": "/ufc-website/static/media/coleman.051a17c0.jpg"
+  },
+  {
+    "revision": "ae705782975e235b1a60aab296b8a0b9",
+    "url": "/ufc-website/static/media/conor.ae705782.jpg"
+  },
+  {
+    "revision": "89addcbad6db1f9b63163cbf49e54a08",
+    "url": "/ufc-website/static/media/conorAldo.89addcba.jpg"
+  },
+  {
+    "revision": "6acb715ff00b4327729eeca6dc50bd57",
+    "url": "/ufc-website/static/media/conorBg.6acb715f.jpg"
+  },
+  {
+    "revision": "226c01f65f83a4bdb5f7efe37be24a95",
+    "url": "/ufc-website/static/media/couture.226c01f6.jpg"
+  },
+  {
+    "revision": "127308df573ec4cacef3fc74775452cc",
+    "url": "/ufc-website/static/media/fastesFinishes.127308df.jpg"
+  },
+  {
+    "revision": "eb251ff3a02d0ba06e364463071e5e96",
+    "url": "/ufc-website/static/media/fightIsland.eb251ff3.jpg"
+  },
+  {
+    "revision": "64f123aaf141cfe6e5fbeb577d2f510a",
+    "url": "/ufc-website/static/media/fightIslandBg.64f123aa.jpg"
+  },
+  {
+    "revision": "785340a0d58c988b12c0cc494b95e253",
+    "url": "/ufc-website/static/media/figueiredo.785340a0.png"
+  },
+  {
+    "revision": "6a24833da22089a13ee7b22cc235f7e6",
+    "url": "/ufc-website/static/media/firstWomen.6a24833d.png"
+  },
+  {
+    "revision": "5f87c6668177713c5387e3ea4e901b5f",
+    "url": "/ufc-website/static/media/gracie.5f87c666.jpg"
+  },
+  {
+    "revision": "5008acdecad5fbfe99ef5109b05fbf49",
+    "url": "/ufc-website/static/media/hughes.5008acde.jpg"
+  },
+  {
+    "revision": "e553930d68e12ba92955c6571ac2808e",
+    "url": "/ufc-website/static/media/justHappend.e553930d.jpg"
+  },
+  {
+    "revision": "35156ee9b982ecc161d915b607b0457f",
+    "url": "/ufc-website/static/media/khabibVsConor.35156ee9.jpg"
+  },
+  {
+    "revision": "d74f9209882246e0e55f9e9380798a7b",
+    "url": "/ufc-website/static/media/liddel.d74f9209.jpg"
+  },
+  {
+    "revision": "82703c05e5c4e0f42b641993d66b37fd",
+    "url": "/ufc-website/static/media/main-bg.82703c05.jpg"
+  },
+  {
+    "revision": "5fa32728273efe2ab76cf73ce3c2de67",
+    "url": "/ufc-website/static/media/mainBg.5fa32728.jpg"
+  },
+  {
+    "revision": "70a41a5bd28721eaabe69f059d484060",
+    "url": "/ufc-website/static/media/mainBg.70a41a5b.png"
+  },
+  {
+    "revision": "c795a4ba0ae04f80cecc168311468b6b",
+    "url": "/ufc-website/static/media/mainBg.c795a4ba.jpg"
+  },
+  {
+    "revision": "eec6a804fa0e3031054cc102ff007f93",
+    "url": "/ufc-website/static/media/miocic.eec6a804.png"
+  },
+  {
+    "revision": "81714bf9840c468b7583e9b1cc84979f",
+    "url": "/ufc-website/static/media/mostDefendedBelt.81714bf9.jpg"
+  },
+  {
+    "revision": "9d3a882ca8e6610d10de946243a6665d",
+    "url": "/ufc-website/static/media/mostFight.9d3a882c.jpg"
+  },
+  {
+    "revision": "473ca612cc81483d9b87d9fdaf69792d",
+    "url": "/ufc-website/static/media/mostFinishes.473ca612.jpg"
+  },
+  {
+    "revision": "d27cb62da2db23ad795eae9e7d616c41",
+    "url": "/ufc-website/static/media/mostKo.d27cb62d.jpg"
+  },
+  {
+    "revision": "b7b451397168d6f7ee3177ed4fa8318c",
+    "url": "/ufc-website/static/media/mostWin.b7b45139.jpg"
+  },
+  {
+    "revision": "ef53037de5b1301cb373351e81461770",
+    "url": "/ufc-website/static/media/news2.ef53037d.jpg"
+  },
+  {
+    "revision": "6592a86207fc0104b8fae9e3521c26e9",
+    "url": "/ufc-website/static/media/newsBlachowicz.6592a862.jpg"
+  },
+  {
+    "revision": "35018ce6daac2259a01a1bc9d774b69f",
+    "url": "/ufc-website/static/media/newsChandler.35018ce6.jpg"
+  },
+  {
+    "revision": "d2393b397c75c1d07a2bbb05d8974283",
+    "url": "/ufc-website/static/media/newsUsman.d2393b39.jpg"
+  },
+  {
+    "revision": "d247a553ea3f719998748506ce5c9654",
+    "url": "/ufc-website/static/media/nigeria.d247a553.jpg"
+  },
+  {
+    "revision": "f44f06b1a6a0d693d0329efb5c10fae2",
+    "url": "/ufc-website/static/media/nunes.f44f06b1.png"
+  },
+  {
+    "revision": "1be25e6bfad651ea48e6e47f80aa70f9",
+    "url": "/ufc-website/static/media/ortiz.1be25e6b.jpg"
+  },
+  {
+    "revision": "4285986ccebd895cedaa9e51774f1407",
+    "url": "/ufc-website/static/media/penn.4285986c.jpg"
+  },
+  {
+    "revision": "2f2d982a36dc6cc9eb894dc0d9dd7d4e",
+    "url": "/ufc-website/static/media/peru.2f2d982a.jpg"
+  },
+  {
+    "revision": "9800b9930a0684227c31ca87a1f1331e",
+    "url": "/ufc-website/static/media/poland.9800b993.jpg"
+  },
+  {
+    "revision": "19c85bb31a9816dac33728b106419307",
+    "url": "/ufc-website/static/media/porierPNG.19c85bb3.png"
+  },
+  {
+    "revision": "f15b0d672f89958188614abb53756fd5",
+    "url": "/ufc-website/static/media/ronda.f15b0d67.jpg"
+  },
+  {
+    "revision": "02478136fc0cc3fe07d79aa2dca0ebf2",
+    "url": "/ufc-website/static/media/russia.02478136.jpg"
+  },
+  {
+    "revision": "03fb0dc017fbede0db5766d78d0b9393",
+    "url": "/ufc-website/static/media/shamrock.03fb0dc0.jpg"
+  },
+  {
+    "revision": "0ebdd4c6f1102dd7b45a4f62197595dc",
+    "url": "/ufc-website/static/media/st-pierre.0ebdd4c6.jpg"
+  },
+  {
+    "revision": "3c77010890a5c0fd63ca5bacf84f2a86",
+    "url": "/ufc-website/static/media/ufc1.3c770108.jpg"
+  },
+  {
+    "revision": "5c4bbdc2b38cb8c2457db659b09b18ac",
+    "url": "/ufc-website/static/media/ufc100.5c4bbdc2.jpg"
+  },
+  {
+    "revision": "164457013dbc4a32c88834c55061b067",
+    "url": "/ufc-website/static/media/ufc12.16445701.jpg"
+  },
+  {
+    "revision": "dbec623472df4da5a222bf3fe45a402a",
+    "url": "/ufc-website/static/media/ufcJapan.dbec6234.jpg"
+  },
+  {
+    "revision": "fae86743fb8b7acadfc877e894545a4a",
+    "url": "/ufc-website/static/media/ufcTuf.fae86743.jpg"
+  },
+  {
+    "revision": "f21adb13cb4f9d72b4394aee39eca910",
+    "url": "/ufc-website/static/media/upNext.f21adb13.jpg"
+  },
+  {
+    "revision": "2b8fb787d42a67c1d92e6e14aee73d11",
+    "url": "/ufc-website/static/media/usa.2b8fb787.jpg"
+  },
+  {
+    "revision": "71ecaf46d5a97ed7ecc790a2fa789a0a",
+    "url": "/ufc-website/static/media/usman.71ecaf46.png"
+  },
+  {
+    "revision": "d3806e54394ec527a2bbed2ce8135eb8",
+    "url": "/ufc-website/static/media/valentina.d3806e54.png"
+  },
+  {
+    "revision": "9121459aa847823f03582a1cc6d78a86",
+    "url": "/ufc-website/static/media/volkanovski.9121459a.png"
+  },
+  {
+    "revision": "c7a3edeb60181211354c387596cbd9f0",
+    "url": "/ufc-website/static/media/weili.c7a3edeb.png"
+  }
+]);
